934d85373e4a1c6fd113365a13940b35777eb068
Lee Hambley <lee.hambley@gmail.com>
2011-08-02 15:28:13 +0800

    Refactor to be a good citizen

--- lib/sunspot_with_kaminari.rb
+++ lib/sunspot_with_kaminari.rb
@@ -1,10 +1,6 @@
-# module SunspotWithKaminari
-# end
-
-module Sunspot
+module SunspotWithKaminari
   module Search
-    class AbstractSearch
-
+    module AbstractSearchInstanceMethods
       # ==== Returns
       #
       # Integer:: Current page number
@@ -12,7 +8,7 @@ module Sunspot
       def current_page
         @query.page
       end
-  
+
       # ==== Returns
       #
       # Integer:: Total number of pages for matching documents
@@ -20,7 +16,7 @@ module Sunspot
       def num_pages
         (total.to_f / @query.per_page).ceil
       end
-  
+
       # ==== Returns
       #
       # Integer:: Number of records displayed per page
@@ -28,14 +24,16 @@ module Sunspot
       def limit_value
         @query.per_page
       end
-      
+
       def empty?
         total == 0
       end
-      
+
       def any?
         total > 0
       end
     end
   end
 end
+
+Sunspot::Search::AbstractSearch.send(:include, SunspotWithKaminari::Search::AbstractSearchInstanceMethods)
